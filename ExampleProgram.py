e808db1191c99ac66f76a00874d26e8c48d90e90
Gothom247 <gnarayanan8756@stu.neisd.net>
2024-10-15 09:28:45 -0700

    Update ExampleProgram.py

--- ExampleProgram.py
+++ ExampleProgram.py
@@ -17,3 +17,6 @@ job_list = {'John': 'Doctor', 'Jane': 'Engineer', 'Jim': 'Teacher'}
 
 for name, job in job_list.items():
   print(f"{name} is a {job}")
+
+Password = 'SuperSecretAdminPass'
+test_password(Password)
